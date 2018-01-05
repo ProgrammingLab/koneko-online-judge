872389f3b61a0dcb8061983c1b6fd767aa91f390
gedorinku <gedorinku@yahoo.co.jp>
2018-01-05 21:25:52 +0900

    認証がおかしかったので修正。

--- app/controllers/session.go
+++ app/controllers/session.go
@@ -15,19 +15,23 @@ func (c Session) Login(email, password string) revel.Result {
 	const message = "メールアドレスまたはパスワードが違います。"
 	c.Validation.Email(email).Message(message)
 
+	if !c.Validation.HasErrors() {
+		session, token, err := models.NewSession(email, password)
+		if err != nil {
+			c.Validation.Error(message)
+		}
+
+		if !c.Validation.HasErrors() {
+			c.Session["userID"] = strconv.Itoa(int(session.User.ID))
+			c.Session["sessionToken"] = token
+		}
+	}
+
 	if c.Validation.HasErrors() {
 		c.Validation.Keep()
 		c.FlashParams()
 		return c.Redirect(App.LoginPage)
 	}
-
-	session, token, err := models.NewSession(email, password)
-	if err != nil {
-		c.Validation.Error(message)
-	}
-
-	c.Session["userID"] = strconv.Itoa(int(session.User.ID))
-	c.Session["sessionToken"] = token
 
 	return c.Redirect(App.Index)
 }
