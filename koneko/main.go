2fc119ae4066a08f332212ff192cc159d6ab841e
gedorinku <gedorinku@yahoo.co.jp>
2018-01-05 19:06:39 +0900

    パスワードのハッシュを生成したりするツールを追加。

---
+++ koneko/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"syscall"
+
+	"github.com/labstack/gommon/log"
+	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func generateSecret() {
+
+}
+
+func passwordDigest(cost int) {
+	fmt.Printf("Digest your password(cost = %v)\n", cost)
+	if cost < bcrypt.MinCost || bcrypt.MaxCost < cost {
+		log.Fatal(bcrypt.InvalidCostError(cost))
+	}
+	fmt.Print("Password: ")
+	password, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println()
+	fmt.Println("Generating...")
+	digest, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(digest))
+}
+
+func help() {
+	fmt.Fprint(os.Stderr, `usage: koneko <command>
+commands:
+	secret		Generate secret key
+	password	Generate password digest
+	help		Print usage
+
+See 'coneko [command] -h' to read about a specific subcommand.
+`)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		help()
+		return
+	}
+
+	passwordFs := flag.NewFlagSet("password", flag.ExitOnError)
+	cost := passwordFs.Int("c", 14, "cost of bcrypt")
+	secretFs := flag.NewFlagSet("secret", flag.ExitOnError)
+
+	var args []string
+	if 2 < len(os.Args) {
+		args = os.Args[2:]
+	}
+	switch os.Args[1] {
+	case "secret":
+		secretFs.Parse(args)
+		if secretFs.Parsed() {
+			generateSecret()
+		}
+	case "password":
+		passwordFs.Parse(args)
+		if passwordFs.Parsed() {
+			passwordDigest(*cost)
+		}
+	case "help":
+		help()
+	default:
+		fmt.Fprintln(os.Stderr, "unknown command: ", os.Args[1])
+		help()
+	}
+}
