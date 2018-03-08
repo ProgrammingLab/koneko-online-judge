50637ac89684342063594b656f66c5b25ed6eb54
gedorinku <gedorinku@yahoo.co.jp>
2018-03-09 00:05:12 +0900

    リファクタリングその1

--- server/models/judgement_job.go
+++ server/models/judgement_job.go
@@ -11,7 +11,10 @@ import (
 )
 
 type judgementJob struct {
-	SubmissionID uint
+	submissionID uint
+	submission   *Submission
+
+	compiled *workers.Worker
 }
 
 const (
@@ -21,151 +24,10 @@ const (
 )
 
 func judge(submissionID uint) {
-	jobs.Now(judgementJob{
-		SubmissionID: submissionID,
+	jobs.Now(&judgementJob{
+		submissionID: submissionID,
+		compiled:     nil,
 	})
-}
-
-func (j judgementJob) Run() {
-	submission := GetSubmission(j.SubmissionID)
-	submission.Status = Judging
-	db.Model(submission).Update("status", submission.Status)
-	submission.FetchLanguage()
-	submission.FetchProblem()
-	submission.FetchJudgeSetResults(false)
-	var (
-		execTime    time.Duration
-		memoryUsage int64
-		point       int
-		finalStatus = Accepted
-	)
-
-	compileWorker, compileRes := compile(submission.SourceCode[:], &submission.Language)
-	if compileWorker == nil || compileRes == nil {
-		finalStatus = UnknownError
-		markAs(submission.JudgeSetResults, finalStatus)
-	} else {
-		defer compileWorker.Remove()
-		logger.AppLog.Debugf("%v %v", compileRes.Status, compileRes.Stderr)
-
-		if compileRes.Status != workers.StatusFinished {
-			finalStatus = CompileError
-			markAs(submission.JudgeSetResults, finalStatus)
-			logger.AppLog.Debugf("compile error: worker status %v", compileRes.Status, compileRes.Stderr)
-		} else {
-			for _, r := range submission.JudgeSetResults {
-				status, t, m := judgeCaseSet(&r, submission, compileWorker)
-				execTime = MaxDuration(execTime, t)
-				memoryUsage = MaxLong(memoryUsage, m)
-				point += r.Point
-				if status == Accepted {
-					continue
-				}
-				finalStatus = status
-			}
-		}
-	}
-
-	submission.Point = point
-	submission.Status = finalStatus
-	submission.ExecTime = execTime
-	submission.MemoryUsage = memoryUsage
-	query := map[string]interface{}{
-		"point":        point,
-		"status":       finalStatus,
-		"exec_time":    execTime,
-		"memory_usage": memoryUsage,
-	}
-	db.Model(&Submission{ID: submission.ID}).Updates(query)
-
-	if submission.Problem.ContestID != nil {
-		p := &submission.Problem
-		p.FetchContest()
-		writer, err := p.Contest.IsWriter(submission.UserID)
-		if err != nil {
-			logger.AppLog.Errorf("error %+v", err)
-			return
-		}
-		if p.Contest.IsOpen(submission.CreatedAt) && !writer {
-			updateScore(submission, *submission.Problem.ContestID)
-		}
-	}
-}
-
-func markAs(setResults []JudgeSetResult, status JudgementStatus) {
-	for _, s := range setResults {
-		s.FetchJudgeResults(false)
-		db.Model(s).Update("status", status)
-		for _, r := range s.JudgeResults {
-			db.Model(r).Update("status", status)
-		}
-	}
-}
-
-func judgeCaseSet(result *JudgeSetResult, submission *Submission, compileWorker *workers.Worker) (JudgementStatus, time.Duration, int64) {
-	result.FetchCaseSet()
-	result.FetchJudgeResults(false)
-
-	setStatus := Accepted
-	var (
-		execTime    time.Duration
-		memoryUsage int64
-	)
-	specialPoint := 0
-	for _, r := range result.JudgeResults {
-		specialPoint += judgeTestCase(&r, submission, compileWorker)
-		execTime = MaxDuration(execTime, r.ExecTime)
-		memoryUsage = MaxLong(memoryUsage, r.MemoryUsage)
-		if r.Status != Accepted {
-			setStatus = r.Status
-		}
-	}
-
-	if setStatus == Accepted {
-		switch submission.Problem.JudgeType {
-		case JudgeTypeNormal:
-			result.Point = result.CaseSet.Point
-		case JudgeTypePrecision:
-			logger.AppLog.Errorf("'JudgeTypePrecision' is not implemented")
-			setStatus = UnknownError
-		case JudgeTypeSpecial:
-			result.Point = specialPoint
-		}
-	}
-
-	result.Status = setStatus
-	result.ExecTime = execTime
-	result.MemoryUsage = memoryUsage
-	query := map[string]interface{}{
-		"point":        result.Point,
-		"status":       setStatus,
-		"exec_time":    execTime,
-		"memory_usage": memoryUsage,
-	}
-	db.Model(&JudgeSetResult{ID: result.ID}).Updates(query)
-
-	return setStatus, execTime, memoryUsage
-}
-
-func judgeTestCase(result *JudgeResult, submission *Submission, compileWorker *workers.Worker) int {
-	result.Status = Judging
-	db.Model(result).Update("status", result.Status)
-	result.FetchTestCase()
-	testCase := &result.TestCase
-
-	res := execSubmission(submission, testCase, compileWorker)
-	var point int
-	result.Status, point = judgeExecResult(res, submission, testCase)
-	result.ExecTime = res.ExecTime
-	result.MemoryUsage = res.MemoryUsage / 1024
-
-	query := map[string]interface{}{
-		"status":       result.Status,
-		"exec_time":    result.ExecTime,
-		"memory_usage": result.MemoryUsage,
-	}
-	db.Model(&JudgeResult{ID: result.ID}).Updates(query)
-	return point
 }
 
 func compile(sourceCode string, language *Language) (*workers.Worker, *workers.ExecResult) {
@@ -191,9 +53,163 @@ func compile(sourceCode string, language *Language) (*workers.Worker, *workers.E
 	return w, res
 }
 
-func execSubmission(submission *Submission, testCase *TestCase, compiled *workers.Worker) *workers.ExecResult {
-	problem := &submission.Problem
-	language := &submission.Language
+func (j *judgementJob) Run() {
+	defer j.Close()
+	j.submission = GetSubmission(j.submissionID)
+	if j.submission == nil {
+		logger.AppLog.Infof("submission(id = %v) is deleted", j.submissionID)
+		return
+	}
+	j.submission.Status = Judging
+	db.Model(j.submission).Update("status", j.submission.Status)
+	j.submission.FetchLanguage()
+	j.submission.FetchProblem()
+	j.submission.FetchJudgeSetResults(false)
+	var (
+		execTime    time.Duration
+		memoryUsage int64
+		point       int
+		finalStatus = Accepted
+	)
+
+	var compileRes *workers.ExecResult
+	j.compiled, compileRes = compile(j.submission.SourceCode[:], &j.submission.Language)
+	if j.compiled == nil || compileRes == nil {
+		finalStatus = UnknownError
+		markAs(j.submission.JudgeSetResults, finalStatus)
+	} else {
+		logger.AppLog.Debugf("%v %v", compileRes.Status, compileRes.Stderr)
+
+		if compileRes.Status != workers.StatusFinished {
+			finalStatus = CompileError
+			markAs(j.submission.JudgeSetResults, finalStatus)
+			logger.AppLog.Debugf("compile error: worker status %v", compileRes.Status, compileRes.Stderr)
+		} else {
+			for _, r := range j.submission.JudgeSetResults {
+				status, t, m := j.judgeCaseSet(&r)
+				execTime = MaxDuration(execTime, t)
+				memoryUsage = MaxLong(memoryUsage, m)
+				point += r.Point
+				if status == Accepted {
+					continue
+				}
+				finalStatus = status
+			}
+		}
+	}
+
+	j.submission.Point = point
+	j.submission.Status = finalStatus
+	j.submission.ExecTime = execTime
+	j.submission.MemoryUsage = memoryUsage
+	query := map[string]interface{}{
+		"point":        point,
+		"status":       finalStatus,
+		"exec_time":    execTime,
+		"memory_usage": memoryUsage,
+	}
+	db.Model(&Submission{ID: j.submission.ID}).Updates(query)
+
+	if j.submission.Problem.ContestID != nil {
+		p := &j.submission.Problem
+		p.FetchContest()
+		writer, err := p.Contest.IsWriter(j.submission.UserID)
+		if err != nil {
+			logger.AppLog.Errorf("error %+v", err)
+			return
+		}
+		if p.Contest.IsOpen(j.submission.CreatedAt) && !writer {
+			updateScore(j.submission, *j.submission.Problem.ContestID)
+		}
+	}
+}
+
+func (j *judgementJob) Close() {
+	if j.compiled == nil {
+		return
+	}
+	j.compiled.Remove()
+}
+
+func markAs(setResults []JudgeSetResult, status JudgementStatus) {
+	for _, s := range setResults {
+		s.FetchJudgeResults(false)
+		db.Model(s).Update("status", status)
+		for _, r := range s.JudgeResults {
+			db.Model(r).Update("status", status)
+		}
+	}
+}
+
+func (j *judgementJob) judgeCaseSet(result *JudgeSetResult) (JudgementStatus, time.Duration, int64) {
+	result.FetchCaseSet()
+	result.FetchJudgeResults(false)
+
+	setStatus := Accepted
+	var (
+		execTime    time.Duration
+		memoryUsage int64
+	)
+	specialPoint := 0
+	for _, r := range result.JudgeResults {
+		specialPoint += j.judgeTestCase(&r)
+		execTime = MaxDuration(execTime, r.ExecTime)
+		memoryUsage = MaxLong(memoryUsage, r.MemoryUsage)
+		if r.Status != Accepted {
+			setStatus = r.Status
+		}
+	}
+
+	if setStatus == Accepted {
+		switch j.submission.Problem.JudgeType {
+		case JudgeTypeNormal:
+			result.Point = result.CaseSet.Point
+		case JudgeTypePrecision:
+			logger.AppLog.Errorf("'JudgeTypePrecision' is not implemented")
+			setStatus = UnknownError
+		case JudgeTypeSpecial:
+			result.Point = specialPoint
+		}
+	}
+
+	result.Status = setStatus
+	result.ExecTime = execTime
+	result.MemoryUsage = memoryUsage
+	query := map[string]interface{}{
+		"point":        result.Point,
+		"status":       setStatus,
+		"exec_time":    execTime,
+		"memory_usage": memoryUsage,
+	}
+	db.Model(&JudgeSetResult{ID: result.ID}).Updates(query)
+
+	return setStatus, execTime, memoryUsage
+}
+
+func (j *judgementJob) judgeTestCase(result *JudgeResult) int {
+	result.Status = Judging
+	db.Model(result).Update("status", result.Status)
+	result.FetchTestCase()
+	testCase := &result.TestCase
+
+	res := j.execSubmission(testCase)
+	var point int
+	result.Status, point = j.judgeExecResult(res, testCase)
+	result.ExecTime = res.ExecTime
+	result.MemoryUsage = res.MemoryUsage / 1024
+
+	query := map[string]interface{}{
+		"status":       result.Status,
+		"exec_time":    result.ExecTime,
+		"memory_usage": result.MemoryUsage,
+	}
+	db.Model(&JudgeResult{ID: result.ID}).Updates(query)
+	return point
+}
+
+func (j *judgementJob) execSubmission(testCase *TestCase) *workers.ExecResult {
+	problem := &j.submission.Problem
+	language := &j.submission.Language
 	cmd := language.GetExecCommandSlice()
 	w, err := workers.NewWorker(imageNamePrefix+language.ImageName, problem.TimeLimit, int64(problem.MemoryLimit*1024*1024), cmd)
 	if err != nil {
@@ -202,7 +218,7 @@ func execSubmission(submission *Submission, testCase *TestCase, compiled *worker
 	}
 	defer w.Remove()
 
-	err = compiled.CopyTo(language.ExeFileName, w)
+	err = j.compiled.CopyTo(language.ExeFileName, w)
 	if err != nil {
 		logger.AppLog.Errorf("exec: docker cp error %+v", err)
 		return nil
@@ -215,24 +231,24 @@ func execSubmission(submission *Submission, testCase *TestCase, compiled *worker
 	return res
 }
 
-func judgeExecResult(res *workers.ExecResult, submission *Submission, testCase *TestCase) (JudgementStatus, int) {
-	problem := &submission.Problem
+func (j *judgementJob) judgeExecResult(res *workers.ExecResult, testCase *TestCase) (JudgementStatus, int) {
+	problem := &j.submission.Problem
 
 	switch problem.JudgeType {
 	case JudgeTypeNormal:
-		return judgeExecResultNormal(res, testCase), 0
+		return j.judgeExecResultNormal(res, testCase), 0
 	case JudgeTypePrecision:
 		logger.AppLog.Errorf("'JudgeTypePrecision' is not implemented")
 		return UnknownError, 0
 	case JudgeTypeSpecial:
-		return judgeExecResultSpecial(res, submission, testCase, problem.JudgementConfig)
+		return j.judgeExecResultSpecial(res, testCase, problem.JudgementConfig)
 	default:
 		logger.AppLog.Errorf("judge type %v is not implemented", problem.JudgeType)
 		return UnknownError, 0
 	}
 }
 
-func judgeExecResultNormal(res *workers.ExecResult, testCase *TestCase) JudgementStatus {
+func (j *judgementJob) judgeExecResultNormal(res *workers.ExecResult, testCase *TestCase) JudgementStatus {
 	if res == nil {
 		return UnknownError
 	}
@@ -257,7 +273,7 @@ func judgeExecResultNormal(res *workers.ExecResult, testCase *TestCase) Judgemen
 	}
 }
 
-func judgeExecResultSpecial(res *workers.ExecResult, submission *Submission, testCase *TestCase, config *JudgementConfig) (JudgementStatus, int) {
+func (j *judgementJob) judgeExecResultSpecial(res *workers.ExecResult, testCase *TestCase, config *JudgementConfig) (JudgementStatus, int) {
 	compiled, compileRes := compile(*config.JudgeSourceCode, config.Language)
 	if compiled == nil || compileRes == nil {
 		return UnknownError, 0
@@ -267,7 +283,7 @@ func judgeExecResultSpecial(res *workers.ExecResult, submission *Submission, tes
 		return CompileError, 0
 	}
 
-	l := submission.Language
+	l := j.submission.Language
 	const (
 		input      = "in"
 		output     = "out"
@@ -284,7 +300,7 @@ func judgeExecResultSpecial(res *workers.ExecResult, submission *Submission, tes
 	w.CopyContentToContainer([]byte(testCase.Input), input)
 	w.CopyContentToContainer([]byte(testCase.Output), output)
 	w.CopyContentToContainer([]byte(res.Stdout), userOutput)
-	w.CopyContentToContainer([]byte(submission.SourceCode), l.FileName)
+	w.CopyContentToContainer([]byte(j.submission.SourceCode), l.FileName)
 
 	judged, err := w.Run("")
 	if err != nil {
