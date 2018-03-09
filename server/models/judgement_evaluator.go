246e7d23938a9dd7170731b3b1dd85762e705493
gedorinku <gedorinku@yahoo.co.jp>
2018-03-09 14:25:26 +0900

    普通のジャッジをいい感じに書いた。

---
+++ server/models/judgement_evaluator.go
@@ -0,0 +1,34 @@
+package models
+
+import "github.com/gedorinku/koneko-online-judge/server/models/workers"
+
+type evaluator interface {
+	next(set *CaseSet) caseSetEvaluator
+	evaluate() (JudgementStatus, int)
+	remove()
+}
+
+type caseSetEvaluator interface {
+	next(res *workers.ExecResult, testCase *TestCase) (JudgementStatus, int)
+	evaluate() (JudgementStatus, int)
+}
+
+func evaluateStatuses(statuses map[JudgementStatus]int) JudgementStatus {
+	max := 0
+	maxSt := Accepted
+	ac := true
+	for k, v := range statuses {
+		if k != Accepted && 0 < v {
+			ac = false
+			break
+		}
+	}
+	for k, v := range statuses {
+		if max < v && (!ac || k != Accepted) {
+			max = v
+			maxSt = k
+		}
+	}
+
+	return maxSt
+}
---
+++ server/models/simple_evaluator.go
@@ -0,0 +1,97 @@
+package models
+
+import (
+	"strings"
+
+	"github.com/gedorinku/koneko-online-judge/server/models/workers"
+)
+
+type simpleEvaluator struct {
+	point    int
+	statuses map[JudgementStatus]int
+	lastSet  *simpleCaseSetEvaluator
+}
+
+func newSimpleEvaluator() *simpleEvaluator {
+	return &simpleEvaluator{
+		statuses: map[JudgementStatus]int{},
+	}
+}
+
+func (e *simpleEvaluator) next(set *CaseSet) caseSetEvaluator {
+	if e.lastSet != nil {
+		st, pt := e.lastSet.evaluate()
+		e.point += pt
+		e.statuses[st]++
+	}
+
+	if set == nil {
+		return nil
+	}
+
+	e.lastSet = newSimpleCaseSetEvaluator(set)
+	return e.lastSet
+}
+
+func (e *simpleEvaluator) evaluate() (JudgementStatus, int) {
+	if e.lastSet == nil {
+		return UnknownError, 0
+	}
+
+	e.next(nil)
+	st := evaluateStatuses(e.statuses)
+	return st, e.point
+}
+
+func (e *simpleEvaluator) remove() {}
+
+type simpleCaseSetEvaluator struct {
+	setPoint int
+	statuses map[JudgementStatus]int
+}
+
+func newSimpleCaseSetEvaluator(set *CaseSet) *simpleCaseSetEvaluator {
+	return &simpleCaseSetEvaluator{
+		setPoint: set.Point,
+		statuses: map[JudgementStatus]int{},
+	}
+}
+
+func (e *simpleCaseSetEvaluator) next(res *workers.ExecResult, testCase *TestCase) (JudgementStatus, int) {
+	// スペシャルジャッジではないので、ケースごとの点数は無視される
+	st, _ := func() (JudgementStatus, int) {
+		if res == nil {
+			return UnknownError, 0
+		}
+
+		switch res.Status {
+		case workers.StatusMemoryLimitExceeded:
+			return MemoryLimitExceeded, 0
+		case workers.StatusTimeLimitExceeded:
+			return TimeLimitExceeded, 0
+		case workers.StatusRuntimeError:
+			return RuntimeError, 0
+		case workers.StatusFinished:
+			if res.Stdout == testCase.Output {
+				return Accepted, 0
+			}
+			if strings.TrimSpace(res.Stdout) == strings.TrimSpace(testCase.Output) {
+				return PresentationError, 0
+			}
+			return WrongAnswer, 0
+		default:
+			return UnknownError, 0
+		}
+	}()
+
+	e.statuses[st]++
+	return st, 0
+}
+
+func (e *simpleCaseSetEvaluator) evaluate() (JudgementStatus, int) {
+	st := evaluateStatuses(e.statuses)
+	if st == Accepted {
+		return st, e.setPoint
+	}
+	return st, 0
+}
