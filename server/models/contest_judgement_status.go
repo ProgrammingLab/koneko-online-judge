a0c14bebe02b1eaca16ffe712650e92207d60db6
gedorinku <gedorinku@yahoo.co.jp>
2018-05-03 16:18:07 +0900

    jsonのフィールド名が大文字から始まっていたので修正。

--- server/models/contest_judgement_status.go
+++ server/models/contest_judgement_status.go
@@ -10,7 +10,7 @@ type ContestJudgementStatus struct {
 	ContestID uint            `gorm:"primary_key" sql:"type:int unsigned" json:"contestID"`
 	UserID    uint            `gorm:"primary_key" sql:"type:int unsigned" json:"userID"`
 	ProblemID uint            `gorm:"not null" json:"problemID"`
-	CreatedAt time.Time       `json:"CreatedAt"`
+	CreatedAt time.Time       `json:"createdAt"`
 	UpdatedAt time.Time       `json:"updatedAt"`
 	Status    JudgementStatus `gorm:"not null; default:'0'" json:"status"`
 	Point     int             `gorm:"not null; default:'0'" json:"point"`
