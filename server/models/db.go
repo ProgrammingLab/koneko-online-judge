a3f66ab81e0b1759c8ce9694377daf17ea8fe687
kp54 <kangpang65@gmail.com>
2018-05-26 10:03:46 +0900

    add settings for python3

--- server/models/db.go
+++ server/models/db.go
@@ -146,6 +146,14 @@ func seedLanguages() {
 			CompileCommand: "g++ -w -lm -std=gnu++2a -O2 -o main.o main.cpp",
 			ExecCommand:    "./main.o",
 		},
+		{
+			ImageName:      "python3",
+			DisplayName:    "Python3 (3.6.4)",
+			FileName:       "main.py",
+			ExeFileName:    "main.py",
+			CompileCommand: "",
+			ExecCommand:    "python3 main.py",
+		},
 	}
 
 	for _, l := range languages {
